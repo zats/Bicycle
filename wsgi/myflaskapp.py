885141b56930cc574dcd2509193be9994d245cfc
Sash Zats <sash@zats.io>
2014-09-03 21:57:03 +0300

    Setting up model

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,36 @@
+import datetime
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
 
 @app.route("/")
 @app.route("/hello")
 def hello():
     return "Hello World!"
+
+@app.route("/env")
+def env():
+    return 
 
 if __name__ == "__main__":
     app.run()
