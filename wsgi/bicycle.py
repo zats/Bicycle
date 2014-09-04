a81c740dcf97d896105a3acbee5ad1a42b40d724
Sash Zats <sash@zats.io>
2014-09-04 23:05:38 +0300

    added logging

--- wsgi/bicycle.py
+++ wsgi/bicycle.py
@@ -91,11 +91,12 @@ def new():
                        'latitude': 33.123, 'longitude': 44.241,
                        'available_bicycles': b2, 'available_poles': 15 - b2, 'capacity': 15}
     }
-    update_with_dictionary(dictionary)
-    return redirect(url_for('hello'))
+    return update_with_dictionary(dictionary)
 
 
 def update_with_dictionary(dictionary):
+    result = ''
+
     station_ids = dictionary.keys()
     hour_of_week = current_hour_of_week()
 
@@ -107,14 +108,14 @@ def update_with_dictionary(dictionary):
     for station in fetched_stations:
         station_dictionary = dictionary_copy[station.id];
         station.update_with_dictionary(station_dictionary)
-        logging.info('updating station for \"' + station.id + '\"')
+        result += 'updating station for \"' + station.id + '\"<br>'
         db.session.add(station)
         del dictionary_copy[station.id]
 
     # Create
     for station_id, station_dictionary in dictionary_copy.items():
         station = Station(station_dictionary)
-        logging.info('creating station for \"' + station_id + '\"')
+        result += 'creating station for \"' + station_id + '\"<br>'
         db.session.add(station)
 
     # Station info
@@ -126,18 +127,18 @@ def update_with_dictionary(dictionary):
     for station_info in fetched_station_infos:
         station_dictionary = dictionary_copy[station.id]
         station_info.update_with_dictionary(station_dictionary)
-        logging.info('updating station info for \"' + station.id + '\"')
+        result += 'updating station info for \"' + station.id + '\"<br>'
         db.session.add(station_info)
         del dictionary_copy[station.id]
 
     # Create
     for station_id, station_dictionary in dictionary_copy.items():
         station_info = StationInfo(station_dictionary, hour_of_week)
-        logging.info('creating station info for \"' + station_id + '\"')
+        result += 'creating station info for \"' + station_id + '\"<br>'
         db.session.add(station_info)
 
     db.session.commit()
-
+    return result
 
 def current_hour_of_week():
     now = time.gmtime(time.time())
