b1ea316036552cc82d38bd9ca45aa5d27d70fce0
Sash Zats <sash@zats.io>
2014-09-11 22:20:04 +0300

    moved models back to the main file

--- wsgi/bicycle.py
+++ wsgi/bicycle.py
@@ -1,19 +1,100 @@
 import logging
+from datetime import datetime
 from flask.ext.sqlalchemy import *
 from flask import Flask, request, flash, url_for, redirect, render_template, abort, jsonify
 from random import randint
 from wsgi.scrapers.telofun_scraper import TelofunScraper
-from wsgi.models.station import Station
-from wsgi.models.station_info import StationInfo
 
 
 CRON_INTERVAL = 2
-
 SERVICES = {'telofun': TelofunScraper.__class__}
+
 
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
 db = SQLAlchemy(app)
+
+
+class Station(db.Model):
+    __tablename__ = 'stations'
+    id = db.Column('id', db.Integer, primary_key=True)
+    station_id = db.Column(db.String)
+    address = db.Column(db.String)
+    description = db.Column(db.String)
+    available_bicycles = db.Column(db.Integer)
+    available_poles = db.Column(db.Integer)
+    capacity = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self, dictionary):
+        expected_keys = ['station_id', 'address', 'description',
+                         'latitude', 'longitude',
+                         'available_bicycles', 'available_poles', 'capacity']
+        for key in expected_keys:
+            setattr(self, key, dictionary[key])
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        pass
+
+    def update_with_dictionary(self, dictionary):
+        expected_keys = ['address', 'description',
+                         'latitude', 'longitude',
+                         'available_bicycles', 'available_poles','capacity']
+        did_update = False
+        for key in expected_keys:
+            if (key in dictionary) and (getattr(self, key, None) != dictionary[key]):
+                setattr(self, key, dictionary[key])
+                did_update = True
+        if did_update:
+            self.updated_at = datetime.utcnow()
+        return did_update
+
+    def to_dict(self):
+        return {
+            'id': self.station_id,
+            'address': self.address,
+            'description': self.description,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'available_bicycles': self.available_bicycles,
+            'available_poles': self.available_poles,
+            'capacity': self.capacity,
+            'created_at': self.created_at.timestamp(),
+            'updated_at': self.updated_at.timestamp()
+        }
+
+
+class StationInfo(db.Model):
+    __tablename__ = 'station_infos'
+    id = db.Column('id', db.Integer, primary_key=True)
+    station_id = db.Column(db.String)
+    available_bicycles = db.Column(db.Float)
+    available_poles = db.Column(db.Float)
+    samples_count = db.Column(db.Integer)
+    hour_of_week = db.Column(db.Float)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __init__(self, dictionary, hour_of_week):
+        self.station_id = dictionary['station_id']
+        self.available_bicycles = dictionary['available_bicycles']
+        self.available_poles = dictionary['available_poles']
+        self.hour_of_week = hour_of_week
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        self.samples_count = 1
+        pass
+
+    def update_with_dictionary(self, dictionary):
+        self.samples_count += 1
+        self.available_bicycles += (dictionary['available_bicycles'] - self.available_bicycles) / float(
+            self.samples_count)
+        self.available_poles += (dictionary['available_poles'] - self.available_poles) / float(self.samples_count)
+        self.updated_at = datetime.utcnow()
+
 
 
 @app.route("/")
