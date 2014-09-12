4ecce96803033c8dcf00412ab320fa6082163ea4
Sash Zats <sash@zats.io>
2014-09-12 22:26:54 +0300

    removed once every 5 minutes rule

--- .openshift/cron/minutely/scrapers.sh
+++ .openshift/cron/minutely/scrapers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
-	touch $OPENSHIFT_DATA_DIR/last_run
-fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
-	rm -f $OPENSHIFT_DATA_DIR/last_run
-	touch $OPENSHIFT_DATA_DIR/last_run
+#if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
+#	touch $OPENSHIFT_DATA_DIR/last_run
+# fi
+# if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
+#	rm -f $OPENSHIFT_DATA_DIR/last_run
+#	touch $OPENSHIFT_DATA_DIR/last_run
+    echo "************ Cronny Started ***************"
     source ${OPENSHIFT_HOMEDIR}/python/virtenv/bin/activate
     python ${OPENSHIFT_REPO_DIR}/wsgi/scrapers.py
-fi
+    echo "************ Cronny Finished ***************"
+# fi
