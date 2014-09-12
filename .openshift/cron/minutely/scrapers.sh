9aa16b82be3ad2ad239929411b7c3a7cc7578ab3
Sash Zats <sash@zats.io>
2014-09-12 22:52:54 +0300

    Changed cron to run once every 2 minutes

--- .openshift/cron/minutely/scrapers.sh
+++ .openshift/cron/minutely/scrapers.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
-#	touch $OPENSHIFT_DATA_DIR/last_run
-# fi
-# if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
-#	rm -f $OPENSHIFT_DATA_DIR/last_run
-#	touch $OPENSHIFT_DATA_DIR/last_run
-    echo "************ Cronny Started ***************"
-#   source ${OPENSHIFT_HOMEDIR}/python/virtenv/bin/activate
+if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
+	touch $OPENSHIFT_DATA_DIR/last_run
+fi
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +1) ]]; then #run every 5 mins
+	rm -f $OPENSHIFT_DATA_DIR/last_run
+	touch $OPENSHIFT_DATA_DIR/last_run
     python ${OPENSHIFT_REPO_DIR}/wsgi/scrapers.py
-    echo "************ Cronny Finished ***************"
-# fi
+fi
