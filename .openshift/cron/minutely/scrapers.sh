e06aa42058ad81cf509bb00a66c6273ec04b9a1a
Sash Zats <sash@zats.io>
2014-09-12 22:25:08 +0300

    Starting cron integration with python

---
+++ .openshift/cron/minutely/scrapers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
+	touch $OPENSHIFT_DATA_DIR/last_run
+fi
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
+	rm -f $OPENSHIFT_DATA_DIR/last_run
+	touch $OPENSHIFT_DATA_DIR/last_run
+    source ${OPENSHIFT_HOMEDIR}/python/virtenv/bin/activate
+    python ${OPENSHIFT_REPO_DIR}/wsgi/scrapers.py
+fi
