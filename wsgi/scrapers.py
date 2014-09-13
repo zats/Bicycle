2da4d7dc0f40323fa83867868fe7049441f54fb8
Sash Zats <sash@zats.io>
2014-09-13 17:41:02 +0300

    fixed scraper script not to fail if one of the scrapers fails

--- wsgi/scrapers.py
+++ wsgi/scrapers.py
@@ -7,5 +7,9 @@ from wsgi.bicycle import SERVICES, scrape_for_service
 print("Hello from the scraper")
 for service in SERVICES.keys():
     print("Scraping for '%s'" % service)
-    scrape_for_service(service)
+    try:
+        scrape_for_service(service)
+    except Exception as e:
+        print("Failed to scrape for '%s'" % service)
+
 
