d12403a27b24d95580f6af283ce4401cce4b7de2
Sash Zats <sash@zats.io>
2014-09-13 11:00:18 +0300

    updated script to run once every 2 minutes (approximately)

--- .openshift/cron/minutely/scrapers.sh
+++ .openshift/cron/minutely/scrapers.sh
@@ -3,7 +3,7 @@
 if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
 	touch $OPENSHIFT_DATA_DIR/last_run
 fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +1) ]]; then #run every 5 mins
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +2) ]]; then #run every 5 mins
 	rm -f $OPENSHIFT_DATA_DIR/last_run
 	touch $OPENSHIFT_DATA_DIR/last_run
     python ${OPENSHIFT_REPO_DIR}/wsgi/scrapers.py
