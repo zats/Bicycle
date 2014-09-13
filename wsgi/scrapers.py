c765bef018d969bada4c28dc64959c761039d9eb
Sash Zats <sash@zats.io>
2014-09-14 00:12:52 +0300

    scraper script ++

--- wsgi/scrapers.py
+++ wsgi/scrapers.py
@@ -6,15 +6,9 @@ from wsgi.scrapers.base_scraper import BaseScraper
 from wsgi.bicycle import *
 
 with app.app_context():
-    print("Hello from the scraper")
-    for scraperClass in BaseScraper.__subclasses__():
+    for service_id, service_dict in SERVICES.items():
+        print("Hello from the scraper %s" % service_id)
         try:
-            print("Scraping for '%s'" % scraperClass)
-            scraper = scraperClass()
-            service_name = scraper.service_name()
-            print("Processing service %s" % service_name)
-            scrape_for_service(service_name, False)
+            scrape_for_service(service_id, False)
         except Exception as e:
             print("Failed to scrape '%s'" % e)
-
-
