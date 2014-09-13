887057d129b9da2654a01b1930ca75012cb963aa
Sash Zats <sash@zats.io>
2014-09-13 19:56:44 +0300

    Added toronto bike share to __init__

--- wsgi/scrapers/__init__.py
+++ wsgi/scrapers/__init__.py
@@ -2,3 +2,4 @@ __author__ = 'zats'
 
 from wsgi.scrapers.telofun_scraper import TelofunScraper
 from wsgi.scrapers.bay_area_bike_share_scraper import BayAreaBikeShareScraper
+from wsgi.scrapers.bike_share_toronto_scraper import BikeShareTorontoScraper
