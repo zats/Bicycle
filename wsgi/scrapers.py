0092933ed920a844aae6ba804111f1c240de03db
Sash Zats <sash@zats.io>
2014-09-23 21:41:49 +0300

    newrelic background tasks 4 of

--- wsgi/scrapers.py
+++ wsgi/scrapers.py
@@ -2,16 +2,13 @@ __author__ = 'zats'
 
 
 import inspect
-import newrelic.agent
 from wsgi.scrapers.base_scraper import BaseScraper
 from wsgi.bicycle import *
 
 with app.app_context():
-    newrelic_app = newrelic.agent.application()
     for service_id, service_dict in SERVICES.items():
         print("Hello from the scraper %s" % service_id)
         try:
-            with newrelic.agent.BackgroundTask(newrelic_app, name='scrape_for_service'):
-                scrape_for_service(service_id, False)
+            scrape_for_service(service_id, False)
         except Exception as e:
             print("Failed to scrape '%s'" % e)
