f2d2149bcd61bb57323de3ddaf35922b95d6cad7
Sash Zats <sash@zats.io>
2014-09-28 22:10:23 +0300

    Commented out all services except for Telofun and Bay Area

--- wsgi/scrapers/__init__.py
+++ wsgi/scrapers/__init__.py
@@ -2,7 +2,7 @@ __author__ = 'zats'
 
 from wsgi.scrapers.telofun_scraper import TelofunScraper
 from wsgi.scrapers.bay_area_bike_share_scraper import BayAreaBikeShareScraper
-from wsgi.scrapers.bike_share_toronto_scraper import BikeShareTorontoScraper
-from wsgi.scrapers.citi_bike_new_york_scraper import CitiBikeNewYorkScraper
+# from wsgi.scrapers.bike_share_toronto_scraper import BikeShareTorontoScraper
+# from wsgi.scrapers.citi_bike_new_york_scraper import CitiBikeNewYorkScraper
 from wsgi.scrapers.divvy_bikes_chicago_scraper import DivvyBikesChicagoScraper
 from wsgi.scrapers.base_scraper import BaseScraper
