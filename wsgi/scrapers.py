4e7a8c7eddca70c7ef893cc46e0ffe4f78024546
Sash Zats <sash@zats.io>
2014-09-30 21:54:46 +0300

    restoring new relic for background tasks

--- wsgi/scrapers.py
+++ wsgi/scrapers.py
@@ -1,10 +1,14 @@
 __author__ = 'zats'
 
 
-import inspect
-from wsgi.scrapers.base_scraper import BaseScraper
-from wsgi.bicycle import *
+import os
+import newrelic.agent
+config_file = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'newrelic.ini')
+newrelic.agent.initialize(config_file)
+application = newrelic.agent.register_application(timeout=10.0)
 
+
+from wsgi.bicycle import *
 with app.app_context():
     for service_id, service_dict in scrapers.items():
         print("Hello from the scraper %s" % service_id)
